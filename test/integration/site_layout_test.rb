0b0a09a6f42b30a3300710758715d79bf71e35a3
Andy Holt <andyholt85@gmail.com>
2018-06-10 23:14:37 -0600

    finished user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,16 +2,35 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = users(:michael)
+  end
+  
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
+    
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+    get login_path
+    assert_select "title", full_title("Log in")
+    
+    get users_path
+    assert_redirected_to login_url
+    get edit_user_path(@user)
+    assert_redirected_to login_url
+    
+    log_in_as(@user)
+    get users_path
+    assert_select "title", full_title("All users")
+    get edit_user_path(@user)
+    assert_select "title", full_title("Edit user")
   end
 end
---
+++ vendor/bundle/ruby/2.3.0/gems/nokogiri-1.10.1/ports/x86_64-pc-linux-gnu/libxml2/2.9.9/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/andy/Documents/TheOdinProject/railsapps/sample_app/vendor/bundle/ruby/2.3.0/gems/nokogiri-1.10.1/ports/x86_64-pc-linux-gnu/libxml2/2.9.9/lib"
+XML2_LIBS="-lxml2 -lz     -lm "
+XML2_INCLUDEDIR="-I/home/andy/Documents/TheOdinProject/railsapps/sample_app/vendor/bundle/ruby/2.3.0/gems/nokogiri-1.10.1/ports/x86_64-pc-linux-gnu/libxml2/2.9.9/include/libxml2"
+MODULE_VERSION="xml2-2.9.9"
+
